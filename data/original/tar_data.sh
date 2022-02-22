de907f066e504a1b6080b5c308e934b8e306987f
Lucas Thompson <dlthompson81@gmail.com>
2022-02-22 12:17:27 -0500

    Updating notebook and cleaning up files

--- data/original/tar_data.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-echo 'Compressing directories...'
-for dir in */; do
-    tar -czf ${dir::-1}.tar.gz ./$dir
-    rm -r $dir
-done
-echo 'Compression complete.'
--- data/original/untar_data.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-echo 'Uncompressing directories...'
-for file in *.tar.gz; do
-    tar -xzf $file
-    rm $file
-done
-echo 'Uncompression complete.'
